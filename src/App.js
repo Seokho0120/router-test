5d6a3159674152181bd4e728273b423a42ac9b79
Seokho <seokho0120@gmail.com>
2023-08-30 17:03:29 +0900

    💫  아울렛 사용

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Videos from './pages/Videos';
+import Root from './pages/Root';
 
 const router = createBrowserRouter([
-  { path: '/', element: <p>Home</p>, errorElement: <p>Not Found</p> },
-  { path: '/videos', element: <p>Videos</p> },
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true, // 기본적으로 첫 페이지 설정
+        element: <Home />,
+      },
+      {
+        path: '/videos',
+        element: <Videos />,
+      },
+    ],
+  },
 ]);
 
 function App() {
